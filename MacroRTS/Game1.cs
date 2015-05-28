6e55b81540a6f8eabd4745f97041a1f9396d1400
Gregory Teuscher <gteuscher@gmail.com>
2015-05-27 23:27:22 -0700

    initial commit of RTS including primitive tiling system

---
+++ MacroRTS/Game1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MacroRTS
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private Texture2D[] bgTextures;
+
+        private Tile[,] gameboard;
+
+        private Random random = new Random();
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            bgTextures = new Texture2D[2];
+            gameboard = new Tile[3, 3];
+            
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+
+            for (int i = 0; i < gameboard.GetLength(0); i++)
+            {
+                for (int j = 0; j< gameboard.GetLength(1); j++)
+                {
+                    int r = random.Next(0, bgTextures.Length);
+                    gameboard[i, j] = new Tile(bgTextures[r]);
+                    gameboard[i, j].pos = new Vector2(i * 128, j * 128);
+                }
+            }
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            bgTextures[0] = Content.Load<Texture2D>("blank");
+            bgTextures[1] = Content.Load<Texture2D>("tower");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Gray);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            for (int i = 0; i < gameboard.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameboard.GetLength(1); j++)
+                {
+                    gameboard[i, j].Draw(spriteBatch);
+                }
+            }
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MacroRTS/Tile.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace MacroRTS
+{
+    class Tile
+    {
+        public Texture2D tileTexture;
+        public Vector2 pos;
+
+        public Tile(Texture2D tileTexture)
+        {
+            this.tileTexture = tileTexture;
+        }
+
+        public void Draw(SpriteBatch s)
+        {
+            s.Draw(tileTexture, pos, Color.White);
+        }
+
+    }
+}
