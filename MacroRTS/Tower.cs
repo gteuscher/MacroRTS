49a6ee40d20fea32406fe939d41eb68c1bbf6e3c
Gregory Teuscher <gteuscher@gmail.com>
2015-05-29 23:32:57 -0700

    started inherited Tower Class

---
+++ MacroRTS/Tower.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace MacroRTS
+{
+    class Tower : Tile
+    {
+        public Tower(Texture2D tileTexture): base (tileTexture)
+        {
+            
+        }
+    }
+}
