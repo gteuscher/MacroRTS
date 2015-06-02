b7e8418aefc1e36574207af343d256139d6fac4e
Gregory Teuscher <gteuscher@gmail.com>
2015-06-02 00:38:25 -0700

    removed unused dependencies

--- MacroRTS/Tile.cs
+++ MacroRTS/Tile.cs
@@ -1,7 +1,5 @@
-﻿using System.Diagnostics;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
 
 namespace MacroRTS
 {
